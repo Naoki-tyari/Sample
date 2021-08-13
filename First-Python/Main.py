23ba826051a6a1b8c6a304c53bd21f7be4936401
Naoki-tyari <88881117+Naoki-tyari@users.noreply.github.com>
2021-08-13 18:12:00 +0900

    add
    create first project

---
+++ First-Python/Main.py
@@ -0,0 +1,13 @@
+x = "3+2"
+y = "5*2"
+a = "5"
+b = "10"
+print(x)
+c = input()
+if(c == a): print("correct!")
+else:print("fuck you")
+
+print(y)
+d = input()
+if(d == b): print("correct!")
+else:print("fuck you")
---
+++ First-Python/janken.py
@@ -0,0 +1,7 @@
+import random
+list = ["グー", "チョキ", "パー"]
+start = input()
+if start = "スタート":
+    print("グーかチョキかパーを入力してね")
+else:
+    print("スタートと入力してね")
